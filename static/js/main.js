3372ea14dc0b96db262e62f5433b4d60bf694a2e
Jawad006 <42263507+mdJawad006@users.noreply.github.com>
2020-11-30 17:37:13 +0600

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@
    "start": "node main.js"
 }
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
